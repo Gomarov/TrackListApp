89f2924deaed28ea9962328fef1ceb2ed4afaa8b
Pavel Gomarov <p.gomarov@icloud.com>
2020-06-04 22:47:50 +0300

    Version 1.1

---
+++ TrackListApp/TrackListViewController.swift
@@ -0,0 +1,86 @@
+//
+//  TrackListViewController.swift
+//  TrackListApp
+//
+//  Created by Павел on 04.06.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import UIKit
+
+class TrackListViewController: UITableViewController {
+
+    let trackList = [
+    "1 - AAA",
+    "2 - BBB",
+    "3 - CCC",
+    "4 - DDD",
+    "5 - EEE",
+    "6 - FFF",
+    "7 - GGG",
+    "8 - HHH",
+    "9 - III",
+    "10 - JJJ",
+    "11 - KKK",
+    ]
+    
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trackList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackName", for: indexPath)
+
+        
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
