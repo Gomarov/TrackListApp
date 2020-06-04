4cded6e520e2276df83571aa63c6908b7d68be67
Pavel Gomarov <p.gomarov@icloud.com>
2020-06-04 23:31:33 +0300

    Version 1.2

--- TrackListApp/TrackListViewController.swift
+++ TrackListApp/TrackListViewController.swift
@@ -11,17 +11,11 @@ import UIKit
 class TrackListViewController: UITableViewController {
 
     let trackList = [
-    "1 - AAA",
-    "2 - BBB",
-    "3 - CCC",
-    "4 - DDD",
-    "5 - EEE",
-    "6 - FFF",
-    "7 - GGG",
-    "8 - HHH",
-    "9 - III",
-    "10 - JJJ",
-    "11 - KKK",
+    "Dave Wincent - Red Eye (Original Mix)",
+    "E-Spectro - End Station (Original Mix)",
+    "John Baptiste - Mycelium (Original Mix)",
+    "Lane 8 - Fingerprint (Original Mix)",
+    "Veerus, Maxie Devine - Nightmare (Original Mix)"
     ]
     
     // MARK: - Table view data source
@@ -33,45 +27,14 @@ class TrackListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TrackName", for: indexPath)
 
-        
+        cell.textLabel?.text = trackList[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        cell.imageView?.image = UIImage(named: trackList[indexPath.row])
 
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+ // MARK: - Table view deligate
 
     /*
     // MARK: - Navigation
